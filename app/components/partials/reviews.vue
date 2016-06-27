f238293e205342ab4912a950b813ede9850bd7c6
Adrian Stoian <stoian.adrian91@gmail.com>
2016-06-27 17:15:15 +0300

    *

---
+++ app/components/partials/reviews.vue
@@ -0,0 +1,26 @@
+<style lang="scss">
+
+  $primary: #38b48d;
+  $secondary: #1a2940;
+  $darkGrey: #3d3d3d;
+  $lightGrey: #787878;
+
+  @mixin fontview {
+    font-family: 'Catamaran', sans-serif;
+    font-weight: normal;
+    -webkit-font-smoothing: antialiased;
+    -moz-font-smoothing: antialiased;
+  }
+
+</style>
+
+<template>
+  <div class="Reviews">
+  </div>
+</template>
+
+<script>
+  // JS HERE
+  export default {
+  };
+</script>
