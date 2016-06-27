775d1bdaf2657b79c2722aeb4100d2afb47b0731
Adrian Stoian <stoian.adrian91@gmail.com>
2016-06-27 14:58:45 +0300

    *

---
+++ app/components/partials/uxs.vue
@@ -0,0 +1,23 @@
+<style lang="scss">
+
+  @mixin fontview {
+    font-family: 'Catamaran', sans-serif;
+    font-weight: normal;
+    -webkit-font-smoothing: antialiased;
+    -moz-font-smoothing: antialiased;
+  }
+</style>
+
+<template>
+  <div class="Uxs">
+    <div class="my-skills col-md-8">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  // JS HERE
+  export default {
+  };
+</script>
